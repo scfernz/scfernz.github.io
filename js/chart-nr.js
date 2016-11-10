daef32f091872cab9718381c006ed00cdd67fea5
Sean Fernandez <scfernz@gmail.com>
2016-11-09 19:57:31 -0800

    Updated ui-challenge

---
+++ js/chart-nr.js
@@ -0,0 +1,16 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'Movie 1',
+      data: [65, 60, 40, 50, 35, 25, 30],
+      backgroundColor: "rgba(190, 190, 190, 0.3)"
+    }, {
+      label: 'Movie 2',
+      data: [48, 48, 60, 40, 57, 38, 30],
+      backgroundColor: "rgba(56, 160, 133, 0.7)"
+    }]
+  }
+});
---
+++ js/chart.js
@@ -0,0 +1,18 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  responsive: 'true',
+  maintainAspectRatio: 'true',
+  data: {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    datasets: [{
+      label: 'Movie 1',
+      data: [65, 60, 40, 50, 35, 25, 30],
+      backgroundColor: "rgba(190, 190, 190, 0.3)"
+    }, {
+      label: 'Movie 2',
+      data: [48, 48, 60, 40, 57, 38, 30],
+      backgroundColor: "rgba(56, 160, 133, 0.7)"
+    }]
+  }
+});
